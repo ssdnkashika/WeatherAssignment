5bfc99011445c29e0227e8480eb514a86979007a
Kashika <kashikassdn12@gmail.com>
2024-10-03 11:40:44 +0530

    Kashika5

--- backend/index.js
+++ backend/index.js
@@ -34,7 +34,7 @@ app.get('/api/weather', async (req, res) => {
 });
 
 // Start the server and log a message
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
